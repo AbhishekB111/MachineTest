f7c951aabfc67b8f75399a47342a508df63ba9a6
Abhishek <abbidkar123@gmail.com>
2023-07-13 17:50:37 +0530

    SQLQuery of the Project

---
+++ SqlQuery_1.sql
@@ -0,0 +1,14 @@
+﻿create database MachineTest
+
+Create table Products
+(
+ProductId int identity(1,1) primary Key,
+ProductName nvarchar(50) null,
+CategoryId int null
+)
+
+Create table Categories
+(
+CategoryId int identity(1,1) primary Key,
+CategoryName nvarchar(50) null
+)
